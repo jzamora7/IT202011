3ae80cc3ef049bdd2304c74a7ea4f40197b302b4
jzamora7 <61848144+jzamora7@users.noreply.github.com>
2020-10-13 21:48:16 -0400

    changing the numbers for our sql tables

--- repo/dbtest/dbtest.php
+++ repo/dbtest/dbtest.php
@@ -6,7 +6,7 @@ $db = getDB();
 //make sure it's set
 if(isset($db)){
     //we'll fetch our sql file from earlier
-    $query = file_get_contents("create_table_users.sql");
+    $query = file_get_contents("001_create_table_users.sql");
     //prepares the query safely to reduce SQL injection (discussed later)
     $stmt = $db->prepare($query);
     //runs the query
