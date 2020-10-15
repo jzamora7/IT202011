049ab097f64a4e0c5352bb6540ae5efb0c177918
Jossie Macbook <jz574@njit.edu>
2020-10-15 14:51:11 -0400

    Relocation from logout to login page pt 2

--- repo/project/logout.php
+++ repo/project/logout.php
@@ -8,9 +8,5 @@ session_unset();
 session_destroy();
 //echo "You're logged out (proof by dumping the session)<br>";
 //echo "<pre>" . var_export($_SESSION, true) . "</pre>";
-if ($done)
-{
-    header("Location: login.php");
-    exit;
-}
+header("Location: login.php");
 ?>
