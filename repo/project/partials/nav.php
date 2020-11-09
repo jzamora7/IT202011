928eeb475433eb1e71d4a15d4a08279202932190
Jossie <jz574@njit.edu>
2020-11-08 20:09:19 -0500

    restructure to Nav Bar

--- repo/project/partials/nav.php
+++ repo/project/partials/nav.php
@@ -6,8 +6,9 @@ require_once(__DIR__ . "/../lib/helpers.php");
 <nav>
     <ul class="nav">
         <?php if (!is_logged_in()): ?>
-            <li><a href="login.php">Login</a></li>
             <li><a href="register.php">Register</a></li>
+            <li><a href="login.php">Login</a></li>
+
         <?php endif; ?>
         <?php if (is_logged_in()): ?>
             <li><a href="profile.php">Profile</a></li>
